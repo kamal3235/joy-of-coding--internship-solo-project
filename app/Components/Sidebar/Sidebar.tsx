cfc967a600a2cad040fc3a5158a7a874417d4004
Abu H Kamal <kamal3235@hotmail.com>
2024-05-24 17:56:25 -0500

    starting new

---
+++ app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useGlobalState } from "@/app/context/globalProvider";
+
+import React from "react";
+import styled from "styled-components";
+
+function Sidebar() {
+  const { theme } = useGlobalState();
+  //console.log(theme);
+
+  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+}
+
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
+
+export default Sidebar;
